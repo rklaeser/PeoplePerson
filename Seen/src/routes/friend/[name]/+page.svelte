3720c97d8f9adc6190efd0fc1ee78e6eef96f65e
Reed Klaeser <reed.klaeser@gmail.com>
2024-11-30 11:23:05 -0800

    Markdown to HTML

--- Seen/src/routes/friend/[name]/+page.svelte
+++ Seen/src/routes/friend/[name]/+page.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
-  export let data: { id: string; name: string };
+  export let data: { id: string; name: string; zip: string; };
+  import snarkdown from 'snarkdown';
+
+  let md = '_this_ is **easy** to `use`.';
+  let html = snarkdown(md);
 </script>
 
 <section>
   <p>{data.name}</p>
+  <p>{data.id}</p>
+  <p>{data.zip}</p>
+  <div>{@html html}</div>
 </section>
 
 <style>
