3ed6c67536addf8a5c9beca8f658fff3dacb244a
Reed Klaeser <reed.klaeser@gmail.com>
2024-11-30 15:05:14 -0800

    Added updated at timestamp

---
+++ Seen/src/lib/db/drizzle/0003_shiny_pretty_boy.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "people" ADD COLUMN "updated_at" timestamp DEFAULT now() NOT NULL;
--- Seen/src/lib/db/schema.ts
+++ Seen/src/lib/db/schema.ts
@@ -1,9 +1,10 @@
-import { integer, pgTable, serial, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { integer, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
 
 export const people = pgTable('people', {
 	id: uuid('id').primaryKey().defaultRandom(),
 	name: text('name').notNull(),
 	zip: integer('zip').notNull(),
 	created_at: timestamp('created_at').notNull().defaultNow(),
+	updated_at: timestamp('updated_at').notNull().defaultNow().$onUpdate(()=> new Date()),
 	body: text('body').notNull()
 });
